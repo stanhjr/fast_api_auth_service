703495202340c6b2b6bcb9f17b27d3f5e097d0ab
Stanislav <stan@MacBook-Air-Stan.local>
2023-08-21 19:28:39 +0300

    update generation

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from fastapi import (
     Request,
 )
 from fastapi.responses import StreamingResponse
-from httpx import AsyncClient
+from httpx import AsyncClient, Timeout
 from prometheus_fastapi_instrumentator import Instrumentator
 from starlette.background import BackgroundTask
 
@@ -40,7 +40,7 @@ async def _reverse_proxy(request: Request):
     headers["host"] = "api.openai.com"
     device_id = headers.get("device-id")
     auth_token = headers.get("authorization")
-    type_query = headers.get("type")
+    type_query = headers.get("type-query")
 
     if not device_id or not auth_token or not type_query:
         raise HTTPException(status_code=400, detail="Not device_id or auth_token")
@@ -52,12 +52,12 @@ async def _reverse_proxy(request: Request):
         raise HTTPException(status_code=401, detail="Unauthorized, token not valid")
 
     headers.pop("device-id")
-    headers.pop("type")
+    headers.pop("type-query")
     headers.pop("authorization")
     headers["Authorization"] = f"Bearer {CHAT_GPT_TOKEN}"
 
     rp_req = CHAT_GPT_SERVER.build_request(
-        request.method, url, headers=headers, content=await request.body()
+        request.method, url, headers=headers, content=await request.body(), timeout=None
     )
 
     rp_resp = await CHAT_GPT_SERVER.send(rp_req, stream=True)
