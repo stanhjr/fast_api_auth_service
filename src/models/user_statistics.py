0755079514bbea8419b4ca1ded09ffb7f79d4f56
Stanislav <stan@MacBook-Air-Stan.local>
2023-09-08 09:41:05 +0300

    add new migrations

--- src/models/user_statistics.py
+++ src/models/user_statistics.py
@@ -5,13 +5,13 @@ from schemas.enums import Type
 
 
 class UserStatistic(Base):
-    __tablename__ = "users_statistics"
+    __tablename__ = "users_statistic"
     device_id: Mapped[str] = mapped_column()
     app_name: Mapped[str] = mapped_column()
     type_query: Mapped[str] = mapped_column()
     tokens: Mapped[int | None]
     type: Mapped[Type] = mapped_column(default=Type.outgoing)
-    chat_model = Mapped[str]
+    chat_model = Mapped[int]
 
     def __str__(self):
         return f"User {self.device_id}"
