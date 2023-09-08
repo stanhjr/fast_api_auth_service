c828ff6e34e8474fe5784dc3c83947ebd5f673ab
Stanislav <stan@MacBook-Air-Stan.local>
2023-09-08 12:47:33 +0300

    update call proxy

--- src/main.py
+++ src/main.py
@@ -31,11 +31,11 @@ async def _reverse_proxy(request: Request):
     headers_service = HeadersService(headers=headers)
     if not headers_service.is_valid():
         raise HTTPException(status_code=400, detail="Not device_id or auth_token")
-    # if not AuthService(
-    #         device_id=headers_service.get_device_id(),
-    #         auth_token=headers_service.get_auth_token()
-    # ).is_authenticate():
-    #     raise HTTPException(status_code=401, detail="Unauthorized, token not valid")
+    if not AuthService(
+            device_id=headers_service.get_device_id(),
+            auth_token=headers_service.get_auth_token()
+    ).is_authenticate():
+        raise HTTPException(status_code=401, detail="Unauthorized, token not valid")
     url = get_url(type_query=headers_service.get_type_query())
     redis_service = RedisService()
     await redis_service.limit_tokens_exceeded_validation(device_id=headers_service.get_device_id(),
